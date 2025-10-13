0498ddc857ad8e1b347c986a330b39772e0a6ce9
Zied <mahmoudkailad4444@gmail.com>
2025-10-13 21:07:02 +0300

    nullptr

--- lcm.js
+++ lcm.js
@@ -21,17 +21,3 @@ function lcm(num1, num2) {
         return b;
     }
 }
-
-/**
- * Calculates the Greatest Common Divisor (GCD) of two numbers using the Euclidean algorithm.
- * @param {number} a The first number.
- * @param {number} b The second number.
- * @returns {number} The GCD of a and b.
- */
-function gcd(a, b) {
-    if (b) {
-        return gcd(b, a % b);
-    } else {
-        return Math.abs(a);
-    }
-}
