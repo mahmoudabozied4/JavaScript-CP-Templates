329e29d515184cdf361b4cd35749ae492761e4ee
Mahmoud Abo-Zied <116457362+mahmoudabozied4@users.noreply.github.com>
2025-10-16 23:03:58 +0300

    Implement Comparator class with comparison methods

---
+++ Comparator.js
@@ -0,0 +1,18 @@
+class Comparator {
+    constructor(compareFunction) {
+        this.compare = compareFunction || Comparator.defaultCompareFunction;
+    }
+
+    static defaultCompareFunction(a, b) {
+        if (a === b) return 0;
+        return a < b ? -1 : 1;
+    }
+
+    equal(a, b) {
+        return this.compare(a, b) === 0;
+    }
+
+    lessThan(a, b) {
+        return this.compare(a, b) < 0;
+    }
+}
