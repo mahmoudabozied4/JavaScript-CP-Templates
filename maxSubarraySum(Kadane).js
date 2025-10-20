af0c39e6f226142a29107d1a73977490790daadf
Mahmoud Abo-Zied <116457362+mahmoudabozied4@users.noreply.github.com>
2025-10-20 03:20:28 +0300

    Implement Kadane's Algorithm for max subarray sum

---
+++ maxSubarraySum(Kadane).js
@@ -0,0 +1,11 @@
+  function maxSubarraySum(arr) {
+        // Kadane's Algorithm
+        let res = arr[0];
+        let maxEnding = arr[0];
+
+        for (let i = 1; i < arr.length; i++) {
+            maxEnding = Math.max(maxEnding + arr[i], arr[i]);
+            res = Math.max(res, maxEnding);
+        }
+        return res;
+    }
